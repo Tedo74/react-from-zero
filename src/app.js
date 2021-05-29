9bb4e36c6384ac2de34ee27c76f6b84154e0dbc9
tedo74 <teddy2@abv.bg>
2021-05-29 15:57:44 +0300

    linters

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
-const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1', {}, 'Adopt me!')
-  );
+/* global React ReactDOM */
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Papper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
