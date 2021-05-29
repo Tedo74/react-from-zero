3f4b725dba2f4edfd21152211f438d0e529c8887
tedo74 <teddy2@abv.bg>
2021-05-29 21:02:36 +0300

    eslint settings

--- src/app.js
+++ src/app.js
@@ -34,4 +34,5 @@ const App = () => {
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
